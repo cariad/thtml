efee74483eac05eb7c8f665e847edc872246b2bc
Cariad Eccleston <cariad@cariad.io>
2021-09-04 13:01:40 +0100

    Use shared test cases

--- tests/test_scope_html_parser.py
+++ tests/test_scope_html_parser.py
@@ -1,5 +1,6 @@
 from pytest import mark
 
+from tests import get_render_cases
 from thtml.options import Scope
 from thtml.scopes import ScopeHtmlParser
 
@@ -15,18 +16,6 @@ def test_get_template(scope: Scope, expect_len: int) -> None:
     assert len(ScopeHtmlParser.get_template(scope)) == expect_len
 
 
-@mark.parametrize(
-    "scope, expect",
-    [
-        (
-            Scope.DOCUMENT,
-            """<!doctype html><html><head><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>cariad/thtml</title><meta name="author" content="cariad/thtml" /></head><body><style type="text/css">.thtml { font-family: monospace; } .thtml .fg-red { color: #f00; } .thtml .fg-green { color: #0f0; }</style><pre class="nohighlight thtml"><code>foo\nbar</code></pre></body></html>""",
-        ),
-        (
-            Scope.FRAGMENT,
-            """<style type="text/css">.thtml { font-family: monospace; } .thtml .fg-red { color: #f00; } .thtml .fg-green { color: #0f0; }</style><pre class="nohighlight thtml"><code>foo\nbar</code></pre>""",
-        ),
-    ],
-)
-def test_render(scope: Scope, expect: str) -> None:
-    assert ScopeHtmlParser("foo\nbar").render(scope) == expect
+@mark.parametrize("body, scope, expect", get_render_cases())
+def test_render(body: str, scope: Scope, expect: str) -> None:
+    assert ScopeHtmlParser(body).render(scope) == expect
