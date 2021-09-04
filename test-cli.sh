0435b6ac1e46557df4bb7f69335db79370549b6a
Cariad Eccleston <cariad@cariad.io>
2021-09-04 13:12:30 +0100

    Add test-cli.sh

---
+++ test-cli.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+set -euo pipefail
+
+assert() {
+  if [[ $1 == $2 ]]; then
+    return
+  fi
+
+  echo "Expected \"$2\" but got \"$1\" 🔥"
+  exit 1
+}
+
+assert $(thtml --version) ${CIRCLE_TAG:-"-1.-1.-1"}
