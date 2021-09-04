5bf33c68c8914706a25f3f5a6546f5ddb857441b
Cariad Eccleston <cariad@cariad.io>
2021-09-04 13:01:29 +0100

    Add render()

--- thtml/__init__.py
+++ thtml/__init__.py
@@ -1,3 +1,10 @@
+from thtml.options import Scope
+from thtml.scopes import ScopeHtmlParser
 from thtml.version import get_version
 
-__all__ = ["get_version"]
+
+def render(body: str, scope: Scope = Scope.DOCUMENT) -> str:
+    return ScopeHtmlParser(body).render(scope)
+
+
+__all__ = ["get_version", "Scope"]
