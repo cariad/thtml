5d45abb831dcc6e4814972de00a80e22bd7fec43
Cariad Eccleston <cariad@cariad.earth>
2021-10-09 13:22:24 +0100

    Include google-fonts.yml and plain.yml (#19)

--- test-cli.sh
+++ test-cli.sh
@@ -12,3 +12,6 @@ assert() {
 }
 
 assert "$(thtml --version)" "${CIRCLE_TAG:-"-1.-1.-1"}"
+assert "$(echo "foo" | thtml)" '<!doctype html><html><head><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>cariad/thtml</title><meta name="author" content="cariad/thtml" /></head><body><style type="text/css">.thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: Fira Code, Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;; letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --white: #CCC; } .thtml-code { font-family: inherit; }</style><pre class="nohighlight thtml"><code class="thtml-code">foo<br /></code></pre></body></html>'
+echo "foo" | thtml --theme google-fonts
+echo "foo" | thtml --theme plain
