a37edd1590ad3b991ce5fb6335b75a7f6881b12f
Cariad Eccleston <cariad@cariad.earth>
2021-10-10 13:22:53 +0100

    Inject `--black` CSS variable in all themes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,26 +1,34 @@
 from io import StringIO
 
+from pytest import mark
+
 from thtml.cli import write_html
 from thtml.options import Scope
 
 
-def test_write_html() -> None:
-    writer = StringIO()
-    write_html(text="\033[1mHello, world!\033[22m", scope=Scope.FRAGMENT, writer=writer)
-    assert (
-        writer.getvalue()
-        == """<style type="text/css">.thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: Fira Code, Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace; letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --white: #CCC; } .thtml-code { font-family: inherit; } .weight-heavy { font-weight: bold; }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="weight-heavy">Hello, world!</span></code></pre>"""
-    )
-
-
-def test_includes_black_var() -> None:
+@mark.parametrize(
+    "theme, expect",
+    [
+        (
+            "default",
+            """<style type="text/css">.thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: Fira Code, Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace; letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --white: #CCC; --black: #000; } .thtml-code { font-family: inherit; } .foreground-black { border-color: var(--black); color: var(--black); } .foreground-default { border-color: var(--white); color: var(--white); }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="foreground-black">Hello, world!</span><span class="foreground-default"></span></code></pre>""",
+        ),
+        (
+            "google-fonts",
+            """<style type="text/css">@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400&display=swap'); .thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: var(--default-font); letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --default-font: 'Roboto Mono'; --white: #CCC; --black: #000; } .thtml-code { font-family: inherit; } .foreground-black { border-color: var(--black); color: var(--black); } .foreground-default { border-color: var(--white); color: var(--white); }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="foreground-black">Hello, world!</span><span class="foreground-default"></span></code></pre>""",
+        ),
+        (
+            "plain",
+            """<style type="text/css">.thtml { --black: #000; } .foreground-black { border-color: var(--black); color: var(--black); } .foreground-default { border-color: var(--white); color: var(--white); }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="foreground-black">Hello, world!</span><span class="foreground-default"></span></code></pre>""",
+        ),
+    ],
+)
+def test_write_html(theme: str, expect: str) -> None:
     writer = StringIO()
     write_html(
         text="\033[30mHello, world!\033[39m",
+        theme=theme,
         scope=Scope.FRAGMENT,
         writer=writer,
     )
-    assert (
-        writer.getvalue()
-        == """<style type="text/css">.thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: Fira Code, Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace; letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --white: #CCC; --black: #000; } .thtml-code { font-family: inherit; } .foreground-black { border-color: var(--black); color: var(--black); } .foreground-default { border-color: var(--white); color: var(--white); }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="foreground-black">Hello, world!</span><span class="foreground-default"></span></code></pre>"""
-    )
+    assert writer.getvalue() == expect
