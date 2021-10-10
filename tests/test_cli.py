f3612467e44d74f6845c7bc4777cd5406a069fbd
Cariad Eccleston <cariad@cariad.earth>
2021-10-10 12:30:38 +0100

    Inject --black CSS variable

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,5 +9,18 @@ def test_write_html() -> None:
     write_html(text="\033[1mHello, world!\033[22m", scope=Scope.FRAGMENT, writer=writer)
     assert (
         writer.getvalue()
-        == """<style type="text/css">.thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: Fira Code, Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;; letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --white: #CCC; } .thtml-code { font-family: inherit; } .weight-heavy { font-weight: bold; }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="weight-heavy">Hello, world!</span></code></pre>"""
+        == """<style type="text/css">.thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: Fira Code, Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace; letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --white: #CCC; } .thtml-code { font-family: inherit; } .weight-heavy { font-weight: bold; }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="weight-heavy">Hello, world!</span></code></pre>"""
+    )
+
+
+def test_includes_black_var() -> None:
+    writer = StringIO()
+    write_html(
+        text="\033[30mHello, world!\033[39m",
+        scope=Scope.FRAGMENT,
+        writer=writer,
+    )
+    assert (
+        writer.getvalue()
+        == """<style type="text/css">.thtml { background: #101013; border-radius: 0.5rem; color: var(--white); font-family: Fira Code, Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace; letter-spacing: 0.03rem; margin-left: auto; margin-right: auto; padding: 2rem; width: max-content; --white: #CCC; --black: #000; } .thtml-code { font-family: inherit; } .foreground-black { border-color: var(--black); color: var(--black); } .foreground-default { border-color: var(--white); color: var(--white); }</style><pre class="nohighlight thtml"><code class="thtml-code"><span class="foreground-black">Hello, world!</span><span class="foreground-default"></span></code></pre>"""
     )
